fc749e9892d6d44ca6fb2d521d9ada4d76ff0492
Etan Cohn <etan.cohn@gmail.com>
2024-11-13 21:24:34 -0500

    stuff

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div class="container">
+        <div class="title">Gal Zeidman PPT Portfolio</div>
+        <iframe class="ppt" src="https://1drv.ms/p/c/608b6f55322da7a4/IQQ8BGeAjicFRaJXNUdWVuSXAXuT_lUrnL1efPwmyyjOMWY?em=2&amp;wdAr=1.7777777777777777" width="476px" height="691px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+        {/* <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a> */}
+        {/* <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+    </div>
+      {/* <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  )
+}
+
+export default App
